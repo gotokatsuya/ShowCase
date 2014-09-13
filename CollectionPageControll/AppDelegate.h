3d6145d7a4a38d63271a6c2a3114a70594e116c0
goto <katsuya.goto@eure.jp>
2014-09-14 01:34:15 +0900

    Initial Commit

---
+++ CollectionPageControll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionPageControll
+//
+//  Created by KatsuyaGoto on 2014/09/14.
+//  Copyright (c) 2014年 KatsuyaGoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionPageControll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionPageControll
+//
+//  Created by KatsuyaGoto on 2014/09/14.
+//  Copyright (c) 2014年 KatsuyaGoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
